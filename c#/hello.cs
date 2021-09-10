daf362d60ab4f9ca2e69d65a5eb6c5f913201fab
Wolf3s <75867486+wolfysdl@users.noreply.github.com>
2021-09-10 20:57:13 -0300

    nova linguagem hehe

---
+++ c#/hello.cs
@@ -0,0 +1,16 @@
+using System;
+
+
+class hello
+{
+     namespace Helloworld
+     {
+     
+        void Main()
+        {
+           Console.WriteLine("Hello world");
+        }
+      
+     }
+
+}
