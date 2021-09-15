f23aec2cb1cb404bbcaa330670cd25e9655f946c
Wolf3s <75867486+wolfysdl@users.noreply.github.com>
2021-09-15 17:48:50 -0300

    Update hello.cs

--- c#/hello.cs
+++ c#/hello.cs
@@ -1,16 +1,14 @@
 using System;
 
-
 class hello
 {
      namespace Helloworld
-     {
-     
+     { 
         void Main()
         {
            Console.WriteLine("Hello world");
         }
-      
+ 
      }
 
 }
